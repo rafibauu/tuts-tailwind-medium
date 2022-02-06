fc1c425113ff9f55a6346c0d311a4973341d6da4
Rafi Bagaskara <rafibauu@gagasimaji.com>
2022-02-06 14:06:38 +0700

    Episode #

--- components/Title.js
+++ components/Title.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Title = () => {
-  return <h1 className="text-8xl">Welcome To tailwind</h1>;
+  return <h1 className="text-8xl text-primary line-clamp-1 btn">
+    Welcome To tailwind Welcome To tailwind Welcome To tailwind
+  </h1>;
 };
 
 export default Title;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#1fb6ff',
+      'secondary': '#7e5bef'
+    },
+    extend: {
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    plugin(function({ addComponents }) {
+      addComponents({
+        '.btn': {
+          padding: '.5rem 1rem !important',
+          borderRadius: '.25rem !important',
+          fontWeight: '600 !important',
+        },
+        // ...
+      })
+    })
+  ],
 }
